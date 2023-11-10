1aa9dff43741a376e50e0c677131690edbc6c09c
misrar09 <misrar09@gmail.com>
2023-11-10 16:03:57 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,34 @@
+let team = [
+    {
+        name: "Wayne Barnett",
+        role: "Founder & CEO",
+        picture: "wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        name: "Angela Caroll",
+        role: "Chief Editor",
+        picture: "angela-caroll-chief-editor.jpg"
+    },
+    {
+        name: "Walter Gordon",
+        role: "Office Manager",
+        picture: "walter-gordon-office-manager.jpg"
+    },
+    {
+        name: "Angela Lopez",
+        role: "Social Media Manager",
+        picture: "angela-lopez-social-media-manager.jpg"
+    },
+    {
+        name: "Scott Estrada",
+        role: "Developer",
+        picture: "scott-estrada-developer.jpg"
+    },
+    {
+        name: "Barbara Ramos",
+        role: "Graphic Designer",
+        picture: "barbara-ramos-graphic-designer.jpg"
+    }
+    
+
+]
